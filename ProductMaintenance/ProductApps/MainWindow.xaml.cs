734f2f47778459ab342dfae388ec77afb9e792be
001175001 <Jesse.HAMILTON-YOUNG@student.tafesa.edu.au>
2022-09-16 10:48:34 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace ProductApps
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalPaymentDeliveryTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
+                totalPaymentWrapTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
             }
             catch (FormatException)
             {
